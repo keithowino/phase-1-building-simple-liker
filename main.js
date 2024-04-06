367e4861cefcc26ca31f7c4f4a69ff04b14c79d7
keithowino <designsolutions1629@gmail.com>
2024-04-06 15:06:04 +0300

    Complete lab.

--- main.js
+++ main.js
@@ -4,60 +4,50 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
-/*
+document.addEventListener("DOMContentLoaded", initInteraction);
 
-When a user clicks on an empty heart:
-  Invoke mimicServerCall to simulate making a server request
-  When the "server" returns a failure status:
-  Respond to the error using a .catch(() => {}) block after your .then(() => {}) block.
-  Display the error modal by removing the .hidden class
-  Display the server error message in the modal
-  Use setTimeout to hide the modal after 3 seconds (add the .hidden class)
-  When the "server" returns a success status:
-  Change the heart to a full heart
-  Add the .activated-heart class to make the heart appear red
+function rejectInteraction(rejectId){
+  const modal = document.querySelector("div.hidden");
+  // Display the error modal by removing the .hidden class.
+  modal.classList.remove("hidden");
+  let errorElement = document.getElementById("modal-message");
+  // Display the server error message in the modal.
+  errorElement.textContent = rejectId.message;
 
-*/
+  // Use setTimeout to hide the modal after 3 seconds (add the .hidden class).
+  setTimeout(function(){
+    modal.classList.add("hidden");
+  }, 3000);
+};
 
-const buttons = document.querySelectorAll(".like-glyph");
+function initInteraction(){
 
-buttons.forEach((button) => {
-  button.addEventListener("click", function (){
-    if(button.innerHTML === EMPTY_HEART){
-      // When a user clicks on an empty heart
-      mimicServerCall()
-      // .then(function(serverMessage){
-      //   alert("You notified the server!");
-      //   alert(serverMessage);
-      //   button.innerHTML = FULL_HEART;
-      //   console.log(button.classList())
-      // })
-      .then(function(){
-        // Change the heart to a full heart
-        // const heart = document.querySelector("footer");
-        // console.log(heart.innerHTML)
-        button.innerHTML = FULL_HEART;
-        button.classlist.add("activated-heart")
+    const likeElements = document.querySelectorAll(".like-glyph");
+
+    likeElements.forEach((likeElement) => {
+      likeElement.addEventListener("click", (e) => {
+        // When a user clicks on an empty heart.
+        if(e.target.innerHTML === EMPTY_HEART){
+          mimicServerCall()
+            // Change the heart to a full heart.
+            // Add the .activated-heart class to make the heart appear red.
+            .then(function(){
+              e.target.innerHTML = FULL_HEART;
+              e.target.classList.add("activated-heart")
+            })
+            // When the "server" returns a failure status.
+            .catch(error => rejectInteraction(error));
+        } else if(e.target.innerHTML === FULL_HEART){
+          mimicServerCall()
+            .then(function(){
+              e.target.innerHTML = EMPTY_HEART;
+              e.target.classList.remove("activated-heart");
+            })
+            .catch(error => rejectInteraction(error));
+        };
       })
-      // When the "server" returns a failure status:
-      .catch(function(error) {
-        const modal = document.querySelector("div.hidden");
-        // Display the error modal by removing the .hidden class
-        modal.classList.remove("hidden");
-        // Display the server error message in the modal
-        let errorElement = document.getElementById("modal-message");
-        errorElement.textContent = error.message;
-        // Use setTimeout to hide the modal after 3 seconds (add the .hidden class)
-        setTimeout(function(){
-          modal.classList.add("hidden");
-        }, 3000);
-      });
-    };
-  })
-});
-
-
-
+    })
+};
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
